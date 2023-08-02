62654c254a6915fc9bb0117e9e24124e6dbc3ed2
c.morais <c.morais1507@gmail.com>
2023-08-02 14:38:28 -0300

    :sparkles: feat: User Search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,58 @@
-'use client'
+import { useEffect, useState } from "react";
+import { Combobox } from "@headlessui/react";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
+import SearchCard from "./SearchCard";
 
-import { MagnifyingGlassIcon } from "@heroicons/react/20/solid"
+interface Person {
+  id: string;
+  name: string;
+  bio: string;
+}
 
 const SearchBar = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [selection, setSelection] = useState('');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network Response was not ok.')
+        }
+        return response.json();
+      })
+      .then(data => {
+        setPeople(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  let filteredPeople: { id: string, name: string }[];
+
+  if (query === '') {
+    filteredPeople = people.map(person => ({ id: person.id, name: person.name }));
+  } else {
+    filteredPeople = people.filter(person => person.name.toLowerCase().includes(query.toLowerCase()))
+      .map(person => ({ id: person.id, name: person.name }));
+  }
+
   return (
-    <form className="join">
-      <input id="search" type="text" className="input input-bordered join-item input-sm md:input-md w-48 md:w-full" />
-      <button type="submit" className="btn btn-square btn-ghost join-item btn-sm md:btn-md"><MagnifyingGlassIcon className="p-1 md:p-2"/></button>
+    <form className="join dropdown">
+      <Combobox value={selection} onChange={setSelection}>
+        <Combobox.Input id="search" type="text" className="input input-bordered join-item input-sm md:input-md w-48 md:w-full" onChange={(event) => setQuery(event.target.value)} />
+        <button type="submit" className="btn btn-square btn-ghost join-item btn-sm md:btn-md"><MagnifyingGlassIcon className="p-1 md:p-2" /></button>
+        <Combobox.Options className="menu menu-sm dropdown-content flex justify-start mt-12 z-[1] p-2 shadow bg-base-100 rounded-box w-fit">
+          {filteredPeople.slice(0, 5).map((person) => (
+            <Combobox.Option key={person.id} value={person.id}>
+              <SearchCard targetUserId={person.id} />
+            </Combobox.Option>
+          ))}
+        </Combobox.Options>
+      </Combobox>
     </form>
   )
 }
---
+++ components/SearchCard.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+
+interface User {
+  id: string;
+  name: string;
+  bio: string;
+  age: number;
+  email: string;
+  emailVerified: string | null;
+  image: string;
+  bannerImage: string;
+}
+
+interface Props {
+  targetUserId: string;
+}
+
+const SearchCard = ({ targetUserId }: Props) => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network Response was not ok.');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setUsers(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const user = users.find((user) => user.id === targetUserId);
+
+  if (!user) {
+    return <span className='loading loading-spinner'></span>;
+  }
+
+  return (
+    <Link href={`/people/${targetUserId}`} className='btn btn-ghost btn-sm h-full w-52'>
+      <div className='flex gap-2 text-clip'>
+        <img src={user.image ?? 'user/png'} alt={user.name} className=' w-5'/>
+        <h1 className='truncate w-40 text-sm'>{user.name}</h1>
+      </div>
+    </Link>
+  );
+};
+
+export default SearchCard;
