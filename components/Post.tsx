2e5c4133144e5a0a1a5b65bfff17d0481b75be2c
c.morais <c.morais1507@gmail.com>
2023-08-07 10:55:05 -0300

    :sparkles: feat: Post UI in the making

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,9 +1,32 @@
+import Link from 'next/link'
 import React from 'react'
+import { FollowButton } from './FollowButton/FollowButton'
+
+interface Props {
+  postId: string
+}
 
 const Post = () => {
   return (
-    <div className='w-full p-6 border-2 border-primary rounded-box'>
-
+    <div className='w-full p-6 border-2 border-primary rounded-box flex flex-col gap-3'>
+      <div className='flex flex-col md:flex-row md:items-center md:justify-between gap-3 md:gap-0'>
+        <div className='flex'>
+          <img src={'/user.png'} alt={'Usuário'} className='avatar rounded-full w-16' />
+          <div className='flex flex-col ml-2'>
+            <Link href={`/people`}>
+              <h1 className='text-lg md:text-xl font-bold'>Carlos Morais</h1>
+            </Link>
+          </div>
+        </div>
+        <FollowButton targetUserId=''/>
+      </div>
+      <div>
+        <p className='md:text-md text-sm text-justify'>
+          Chasing sunsets, collecting memories. 🌅✨ Laugh a little louder, love a little deeper. 💖 Embracing every moment and creating my own sunshine. ☀️💫 #ChasingDreams #JoyfulLife
+        </p>
+      </div>
+      <div>
+      </div>
     </div>
   )
 }
