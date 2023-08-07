567f4482ac995de623aa4bac98e3b18b3d0362b2
c.morais <c.morais1507@gmail.com>
2023-08-07 10:34:50 -0300

    :sparkles: feat: Feed Page created

---
+++ app/feed/page.tsx
@@ -0,0 +1,21 @@
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid"
+import { prisma } from "@/lib/prisma"
+import Post from "@/components/Post";
+
+export const dynamic = 'force-dynamic'
+
+export default async function People() {
+  const posts = await prisma.post.findMany();
+
+  return(
+    <main className=" mt-[65px] px-12 pt-6">
+      <div className="flex gap-3 text-primary prose prose-h1:text-primary items-center justify-start">
+        <PaperAirplaneIcon className="h-[30px] lg:h-[60px]"/>
+        <h1 className="text-3xl lg:text-6xl">Feed</h1>
+      </div>
+        <div className="w-full p-6 flex flex-col gap-5">
+          <Post />
+        </div>
+    </main>
+  )
+}
---
+++ components/Post.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div className='w-full p-6 border-2 border-primary rounded-box'>
+
+    </div>
+  )
+}
+
+export default Post
