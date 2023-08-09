191a8dac10500677e2698aa45c126ae17333f411
c.morais <c.morais1507@gmail.com>
2023-08-09 12:25:44 -0300

    :bug: fix: Post Issue

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -3,38 +3,24 @@ import { NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { prisma } from "@/lib/prisma";
 
-export default async function handler(req: Request, res: Response) {
-  if (req.method === 'GET') {
-    const posts = await prisma.post.findMany();
-    return NextResponse.json(posts);
-  } else if (req.method === 'POST') {
-    const session = await getServerSession(authOptions);
+export async function POST(req: Request, res: Response) {
+  const session = await getServerSession(authOptions);
     const currentUserId = await prisma.user
       .findUnique({ where: { email: session?.user?.email! } })
       .then((user) => user?.id!);
 
-    try {
-      const requestBody = await req.json();
-      const content = requestBody.post;
-
-      if (!content) {
-        return new NextResponse("Content is missing in the request body", { status: 400 });
-      }
-
-      console.log(content)
+      const requestBody = JSON.parse(await req.text());
+      const { post } = requestBody;
 
       const record = await prisma.post.create({
         data: {
-          content: content,
+          content: post,
           likes: 0,
           reposts: 0,
           userId: currentUserId
         }
       });
 
-      return new NextResponse(JSON.stringify(record), { status: 201 });
-    } catch (error) {
-      return new NextResponse("Invalid JSON in request body", { status: 400 });
-    }
-  }
+  return NextResponse.json(record);
 }
+
--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -7,29 +7,37 @@ const PostForm = () => {
 
   const sendPost = async (e: any) => {
     e.preventDefault();
-
-    await fetch('/api/post', {
+  
+    const response = await fetch('/api/post', {
       method: 'POST',
       body: JSON.stringify({ post }),
       headers: {
         'Content-Type': 'application/json'
       }
     });
+
+    if (!response.ok) {
+      console.error('Request failed with status:', response.status);
+      return;
+    }
+  
+    const data = response
+    console.log(data);
   }
 
   return (
-    <form className="flex flex-col gap-3 max-h-64" onSubmit={sendPost}>
-      <textarea
+    <form className="flex flex-col gap-3 max-h-64">
+      <input
+        type="text"
         name="post"
         id="post"
-        cols={25}
-        rows={10}
+
         maxLength={250}
         className="textarea textarea-primary"
         value={post}
         onChange={(e) => setPost(e.target.value)}
       />
-      <button type="submit" className="btn btn-primary">
+      <button type="submit" onClick={sendPost} className="btn btn-primary">
         Enviar
       </button>
     </form>
